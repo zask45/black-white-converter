ab6548e55b2fecf570b40ccb307fa59852689e02
zask45 <zaschakeysha@gmail.com>
2025-02-26 00:17:11 +0700

    upload image to front end

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,7 +1,12 @@
-import './App.css'
-import './components/UploadImage'
+import { useState } from "react"
 
 const App = () => {
+  const [image, setImage] = useState()
+
+  const uploadImage = (e) => {
+    setImage(URL.createObjectURL(e.target.files[0]))
+  }
+
   return (
     <>
       <div class="flex items-center justify-center w-full">
@@ -13,9 +18,10 @@ const App = () => {
               <p class="mb-2 text-sm text-gray-500 dark:text-gray-400"><span class="font-semibold">Click to upload</span> or drag and drop</p>
               <p class="text-xs text-gray-500 dark:text-gray-400">SVG, PNG, JPG or GIF (MAX. 800x400px)</p>
           </div>
-          <input id="dropzone-file" type="file" class="hidden" />
+          <input id="dropzone-file" type="file" accept="image/*" class="hidden" onChange={uploadImage} />
         </label>
       </div> 
+      <img src={image} />
       <div class="flex flex-col items-center">
           <button class="bg-blue-600 font-semibold text-white px-2 py-2 rounded">Upload gambar</button>
       </div>
