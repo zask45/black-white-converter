debcebcf2ca1a6bbfdc08693c584dd91e4d2b247
zask45 <zaschakeysha@gmail.com>
2025-02-25 23:41:54 +0700

    update function

--- client/src/components/UploadImage.jsx
+++ client/src/components/UploadImage.jsx
@@ -1,4 +1,4 @@
-const UploadImage = () => { 
+const UploadImage = () => {
     <div class="flex items-center justify-center w-full">
         <label for="dropzone-file" class="flex flex-col items-center justify-center w-full h-64 border-2 border-gray-300 border-dashed rounded-lg cursor-pointer bg-gray-50 dark:hover:bg-gray-800 dark:bg-gray-700 hover:bg-gray-100 dark:border-gray-600 dark:hover:border-gray-500 dark:hover:bg-gray-600">
             <div class="flex flex-col items-center justify-center pt-5 pb-6">
