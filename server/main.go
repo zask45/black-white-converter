604a056e0d8ab9d841a453318868b3c56a709db4
zask45 <zaschakeysha@gmail.com>
2025-02-26 15:26:09 +0700

    add processImage

--- server/main.go
+++ server/main.go
@@ -2,15 +2,69 @@ package main
 
 import (
 	"fmt"
+	"image"
+	"image/color"
+	"image/jpeg"
+	"image/png"
+	"log"
 	"net/http"
+	"os"
+	"path/filepath"
+	"strings"
 
 	"github.com/gin-gonic/gin"
 )
 
 func postHandler(c *gin.Context) {
 	file, _ := c.FormFile("file")
-	c.SaveUploadedFile(file, "uploads/"+file.Filename)
+
+	filename := file.Filename
+	c.SaveUploadedFile(file, filename)
+
+	processImage(c, filename)
 	c.JSON(http.StatusOK, gin.H{"message": "Upload sukses"})
+}
+
+func processImage(c *gin.Context, filename string) {
+	// Open file
+	file, err := os.Open(filename)
+	if err != nil {
+		throwMessage(c, "Gagal membuka gambar")
+	}
+	defer file.Close()
+
+	// Decode image
+	img, _, err := image.Decode(file)
+	if err != nil {
+		throwMessage(c, "Format gambar tidak didukung")
+	}
+
+	// Convert ke black and white
+	result := convertToBlackAndWhite(img, 128)
+
+	// Create new file to save result
+	outFile, err := os.Create(filename + "_black_and_white")
+	if err != nil {
+		throwMessage(c, err.Error())
+	}
+	defer outFile.Close()
+
+	// Save result to PNG
+	err = png.Encode(outFile, result)
+	if err != nil {
+		throwMessage(c, "Gagal mengencoding gambar")
+	}
+
+	fmt.Println("Gambar berhasil diproses")
+}
+
+func convertToBlackAndWhite(img image.Image, threshold uint8) *image.Gray {
+
+}
+
+func throwMessage(c *gin.Context, message string) {
+	c.JSON(400, gin.H{"message": message})
+	return
 }
 
 func main() {
