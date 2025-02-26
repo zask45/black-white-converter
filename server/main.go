6b09210fdac33e8636d89960950242378cc3a696
zask45 <zaschakeysha@gmail.com>
2025-02-26 14:35:42 +0700

    add postHandler

--- server/main.go
+++ server/main.go
@@ -2,21 +2,20 @@ package main
 
 import (
 	"fmt"
+	"net/http"
 
 	"github.com/gin-gonic/gin"
 )
 
-func rootHandler(c *gin.Context) {
-	c.JSON(200, gin.H{
-		"Name":    "Yuta Okkotsu",
-		"Age":     20,
-		"Address": "Tokyo",
-	})
+func postHandler(c *gin.Context) {
+	file, _ := c.FormFile("file")
+	c.SaveUploadedFile(file, "uploads/"+file.Filename)
+	c.JSON(http.StatusOK, gin.H{"message": "Upload sukses"})
 }
 
 func main() {
 	router := gin.Default()
-	router.GET("/", rootHandler)
+	router.POST("/upload", postHandler)
 	fmt.Println("http://localhost:8080")
 	router.Run(":8080")
 }
