08a8d2856a581926e59aa0e1997381b2219d5f52
zask45 <zaschakeysha@gmail.com>
2025-03-01 21:10:34 +0700

    fix preview image

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -5,8 +5,12 @@ const App = () => {
   const [selectedFile, setSelectedFile] = useState()
 
   const handleChange = (e) => {
-    setImage(URL.createObjectURL(e.target.files[0]))
-    setSelectedFile(e.target.files[0])
+    const file = e.target.files[0]
+
+    setImage(URL.createObjectURL(file))
+    setSelectedFile(file)
+
+    document.getElementById("dropzone").style.display = "none"
   }
 
   const processImage = async() => {
@@ -40,22 +44,21 @@ const App = () => {
   return (
     <>
       <div class="flex items-center justify-center w-screen h-[45rem] px-10 py-8">
-        <label for="dropzone-file" class="flex flex-col items-center justify-center w-full h-full border-2 border-gray-300 border-dashed rounded-lg cursor-pointer bg-gray-50 dark:hover:bg-gray-800 dark:bg-gray-700 hover:bg-gray-100 dark:border-gray-600 dark:hover:border-gray-500 dark:hover:bg-gray-600">
+        <label id="dropzone" for="dropzone-file" class="flex flex-col items-center justify-center w-full h-full border-2 border-gray-300 border-dashed rounded-lg cursor-pointer bg-gray-50 dark:hover:bg-gray-800 dark:bg-gray-700 hover:bg-gray-100 dark:border-gray-600 dark:hover:border-gray-500 dark:hover:bg-gray-600">
           <div class="flex flex-col items-center justify-center pt-5 pb-6">
               <svg class="w-8 h-8 mb-4 text-gray-500 dark:text-gray-400" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 20 16">
                   <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M13 13h3a3 3 0 0 0 0-6h-.025A5.56 5.56 0 0 0 16 6.5 5.5 5.5 0 0 0 5.207 5.021C5.137 5.017 5.071 5 5 5a4 4 0 0 0 0 8h2.167M10 15V6m0 0L8 8m2-2 2 2"/>
               </svg>
               <p class="mb-2 text-sm text-gray-500 dark:text-gray-400"><span class="font-semibold">Click to upload</span> or drag and drop</p>
-              <p class="text-xs text-gray-500 dark:text-gray-400">PNG FILE</p>
+              <p id="file-info" class="text-xs text-gray-500 dark:text-gray-400">PNG FILE</p>
           </div>
 
           <input id="dropzone-file" type="file" accept="image/*" class="hidden" onChange={handleChange} />
 
         </label>
-      </div> 
 
-      <img src={image} />
-      <img id="preview" />
+        <img id="preview" src={image} class="h-full" />
+      </div> 
       
       <div class="flex flex-col items-center">
           <button class="bg-blue-600 font-semibold text-white px-2 py-2 rounded" onClick={processImage}>Upload gambar</button>
