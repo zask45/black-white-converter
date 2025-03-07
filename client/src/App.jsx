79e5a730119a8008f2e46e41bb23cb4aefb00ea0
zask45 <zaschakeysha@gmail.com>
2025-03-07 23:15:48 +0700

    enable and disable button

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,11 +3,17 @@ import { useState } from "react"
 const App = () => {
   const [image, setImage] = useState()
   const [selectedFile, setSelectedFile] = useState(null)
-  const [showDropzone, setShowDropzone] = useState(true)
+  const [isReadyToConvert, setIsReadyToConvert] = useState(true)
   const [isConverted, setIsConverted] = useState(false)
 
   if (isConverted) {
     document.getElementById("action-btn").textContent = "Download"
+  }
+
+  if (!isReadyToConvert) {
+    document.getElementById("action-btn").classList.remove("cursor-not-allowed")
+    document.getElementById("action-btn").classList.remove("bg-slate-400")
+    document.getElementById("action-btn").classList.add("bg-blue-400")
   }
 
   const handleChange = (e) => {
@@ -16,7 +22,7 @@ const App = () => {
     setImage(URL.createObjectURL(file))
     setSelectedFile(file)
 
-    setShowDropzone(false)
+    setIsReadyToConvert(false)
   }
 
   const handleClick = () => {
@@ -69,15 +75,15 @@ const App = () => {
 
   return (
     <>
-      <div class="flex items-center justify-center w-95 h-[33rem] px-6 pt-4 pb-4">
-        {showDropzone && (
+      <div class="flex items-center justify-center w-95 h-[45rem] px-6 pt-4 pb-4">
+        {isReadyToConvert && (
           <label id="dropzone" for="dropzone-file" class="flex flex-col items-center justify-center w-full h-full border-2 border-gray-300 border-dashed rounded-lg cursor-pointer bg-gray-50 dark:hover:bg-gray-800 dark:bg-gray-700 hover:bg-gray-100 dark:border-gray-600 dark:hover:border-gray-500 dark:hover:bg-gray-600">
             <div class="flex flex-col items-center justify-center pt-5 pb-6">
                 <svg class="w-8 h-8 mb-4 text-gray-500 dark:text-gray-400" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 20 16">
                     <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M13 13h3a3 3 0 0 0 0-6h-.025A5.56 5.56 0 0 0 16 6.5 5.5 5.5 0 0 0 5.207 5.021C5.137 5.017 5.071 5 5 5a4 4 0 0 0 0 8h2.167M10 15V6m0 0L8 8m2-2 2 2"/>
                 </svg>
-                <p class="mb-2 text-sm text-gray-500 dark:text-gray-400"><span class="font-semibold">Click to upload</span> or drag and drop</p>
-                <p id="file-info" class="text-xs text-gray-500 dark:text-gray-400">PNG FILE</p>
+                <p class="mb-2 text-md text-gray-500 dark:text-gray-400"><span class="font-semibold">Click to upload</span> or drag and drop</p>
+                <p id="file-info" class="text-sm text-gray-500 dark:text-gray-400">PNG FILE</p>
             </div>
 
             <input id="dropzone-file" type="file" accept="image/png" class="hidden" onChange={handleChange} />
@@ -86,10 +92,11 @@ const App = () => {
         )}
 
         <img id="preview" src={image} class="h-full"/>
+
       </div> 
       
       <div class="flex flex-col items-center">
-          <button id="action-btn" class="bg-slate-300 font-semibold text-white px-2 py-2 rounded" onClick={handleClick}>Convert image</button>
+          <button id="action-btn" class="bg-slate-400 font-semibold text-white px-2 py-2 rounded cursor-not-allowed" onClick={handleClick} disabled={isReadyToConvert}>Convert image</button>
       </div>
     </>
   )
